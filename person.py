78d066d39add04c038a44632fe2fe2b72aebda18
Britta Carlsson <britta.carlsson@studerande.plushogskolan.se>
2021-10-28 10:58:23 +0200

    Now persons is back

---
+++ person.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self, first_name, relation=None, last_name=None, nickname=None):
+        self.first_name = first_name
+        self.relation = relation
+        self.last_name = last_name
+        self.nickname = nickname
